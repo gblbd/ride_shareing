539bd740398fd709e45289322ce15a45101c9e4c
gblbd <gbl.devs@gmail.com>
2023-06-03 18:55:48 +0600

    ✅✅✅ Update 6:55PM

--- lib/Pages/HomePage/Account&Settings/NotificationScreen.dart
+++ lib/Pages/HomePage/Account&Settings/NotificationScreen.dart
@@ -1,8 +1,11 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class NotificationScreen extends StatefulWidget {
-  const NotificationScreen({Key? key}) : super(key: key);
+  final String senderPhoneNumber;
+  const NotificationScreen({Key? key, required this.senderPhoneNumber}) : super(key: key);
 
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
@@ -11,6 +14,17 @@ class NotificationScreen extends StatefulWidget {
 class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
+    Query dbref=FirebaseDatabase.instance.ref("User_profile").child(widget.senderPhoneNumber).child("user").child("Courier");
+
+
+    // dbref.onValue.listen((event) {
+    //   DataSnapshot ssnapshot = event.snapshot;
+    //   String key = ssnapshot.key.toString();
+    //   triggerNotification(ssnapshot.key.toString());
+    // });
+
+
+
     return Scaffold(
       appBar: AppBar(
         elevation: 1.0,
@@ -26,14 +40,81 @@ class _NotificationScreenState extends State<NotificationScreen> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            // child: FirebaseAnimatedList(
-            //   physics: ScrollPhysics(),
-            //   shrinkWrap: true,
-            //   query: ,
-            // ),
+            child: FirebaseAnimatedList(
+              physics: ScrollPhysics(),
+              shrinkWrap: true,
+              query: dbref,
+              reverse: true,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
+                dbref.onValue.listen((event) {
+                  String key = snapshot.key.toString();
+                  triggerNotification(key);
+                });
+
+
+                return Card(
+                  child: ListTile(
+                    onTap: (){
+
+                    },
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.green.shade50,
+                        child: Icon(Icons.check,color: Colors.green,size: 20,)),
+                    title: RichText(text: TextSpan(
+                      text: 'We have received your courier order ${snapshot.key.toString()}. In the next few hours, we will collect your package from your given address.',
+                      style: TextStyle(color: Colors.black)
+                    ),
+                    ),
+                    subtitle: Text('${DateTime.now()}'),
+                  ),
+
+                );
+              },
+            ),
           ),
         ],
       ),
     );
+  }
+
+
+  triggerNotification(String key)
+  {
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 10,
+            channelKey: 'basic_channel',
+            title: 'Thanks for your courier order $key',
+            body: 'We have received your courier order $key. In the next few hours, we will collect your package from your given address.',
+
+        )
+    );
+  }
+  @override
+  void initState() {
+
+
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed){
+
+      if(!isAllowed)
+      {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+
+    });
+    super.initState();
+
+    //
+    // rf.child("notification").onChildAdded.listen((event) {
+    //   showNotification(event.snapshot.value);
+    // });
+  }
+
+  Future<void>onSelectNotification(String payload)async{
+
+  }
+
+  Future<void>showNotification(String data)async{
+
   }
 }
