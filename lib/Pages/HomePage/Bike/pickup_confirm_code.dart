ac7049ece68c35ec825f94576596d020fd325e9f
recen <recentnafimahmed@gmail.com>
2023-06-03 12:10:21 +0600

    nafim bike design done  12:10 3/06/2023

---
+++ lib/Pages/HomePage/Bike/pickup_confirm_code.dart
@@ -0,0 +1,63 @@
+
+
+
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PickupConfirmPage extends StatelessWidget{
+
+  int randNumb(){
+
+    int i=Random().nextInt(999999);
+    if(i<100000){
+      i=i+100000;
+    }
+
+    return i;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Confirm picKup"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Your Pickup code is :",
+
+              style: GoogleFonts.openSans(
+                fontSize: 21,
+                fontWeight: FontWeight.bold
+              ),
+
+
+            ),
+            Text("${randNumb()}",
+
+              style: GoogleFonts.openSans(
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),
+
+            ),
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+
+}
