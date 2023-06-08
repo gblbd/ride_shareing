6b8b021308a5080d7a665af0954dee9d592d99db
recen <recentnafimahmed@gmail.com>
2023-06-08 12:01:37 +0600

    nafim all done2 12:01 pm 8/06/2023

--- lib/Pages/HomePage/Assistants/assistantMethods.dart
+++ lib/Pages/HomePage/Assistants/assistantMethods.dart
@@ -1,10 +1,10 @@
-import 'package:geolocator/geolocator.dart';
+//import 'package:geolocator/geolocator.dart';
 import 'package:ride_sharing/Pages/HomePage/Assistants/RequestAssistant.dart';
 
 class AssistantMethods{
-  static Future<String>searchCoordinateAddress(Position position)async{
-    String PlaceAddress="";
-    String url="https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=AIzaSyBsPxSFf2or6oZnbq7urgrxlakTiVqTmjQ";
-    var response=await RequestAssistant.getRequest(url);
-  }
+  // static Future<String>searchCoordinateAddress(Position position)async{
+  //   String PlaceAddress="";
+  //   String url="https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=AIzaSyBsPxSFf2or6oZnbq7urgrxlakTiVqTmjQ";
+  //   var response=await RequestAssistant.getRequest(url);
+  // }
 }
