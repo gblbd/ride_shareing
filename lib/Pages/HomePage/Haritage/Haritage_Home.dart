c7e7f160daaa7ed2517afd90b204e5959c87dbfa
recen <recentnafimahmed@gmail.com>
2023-07-27 10:00:07 +0600

    nafim  tourist place list  created  9:59 am 27/07/2023

--- lib/Pages/HomePage/Haritage/Haritage_Home.dart
+++ lib/Pages/HomePage/Haritage/Haritage_Home.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ride_sharing/Pages/HomePage/Haritage/heritage_list.dart';
+import 'package:sizer/sizer.dart';
 
 class HariTageHome extends StatelessWidget{
   @override
@@ -12,34 +14,84 @@ class HariTageHome extends StatelessWidget{
       body: Column(
         children: [
 
-          ListTile(
-            leading: Image.asset('assets/images/itinerary.png',),
-            title: const Text('Make a tour plan',
-                style: TextStyle(color: Colors.black,fontSize: 16,)),
-            subtitle: const Text('Make a easy, meaningful, efficient, hassle free and enjoyable tour',
-                style: TextStyle(color: Colors.grey,fontSize: 13,)),
-            trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
-          ),
 
 
-          ListTile(
-            leading: Image.asset('assets/images/mountains.png',),
-            title: const Text('Our populer tourist destinations',
-                style: TextStyle(color: Colors.black,fontSize: 16,)),
-            subtitle: const Text('The natural and historical beauty of the Bangladesh',
-                style: TextStyle(color: Colors.grey,fontSize: 13,)),
-            trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("assets/images/travel.png",
+              height: 30.h,
+              width: 80.w,
+            ),
           ),
 
 
 
-          ListTile(
-            leading: Image.asset('assets/images/customer-service.png',),
-            title: const Text('Help and Support',
-                style: TextStyle(color: Colors.black,fontSize: 16,)),
-            subtitle: const Text('always active for safe and hassle free travel',
-                style: TextStyle(color: Colors.grey,fontSize: 13,)),
-            trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
+
+
+
+          Container(
+            margin: const EdgeInsets.only(left: 8,right: 8,top: 8),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Image.asset('assets/images/itinerary.png',),
+                  title: const Text('Make a tour plan',
+                      style: TextStyle(color: Colors.black,fontSize: 16,)),
+                  subtitle: const Text('Make a easy, meaningful, efficient, hassle free and enjoyable tour',
+                      style: TextStyle(color: Colors.grey,fontSize: 13,)),
+                  trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
+                ),
+              ),
+            ),
+          ),
+
+
+          Container(
+            margin: const EdgeInsets.only(left: 8,right: 8,top: 8),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Image.asset('assets/images/mountains.png',),
+                  title: const Text('Our populer tourist destinations',
+                      style: TextStyle(color: Colors.black,fontSize: 16,)),
+                  subtitle: const Text('The natural and historical beauty of the Bangladesh',
+                      style: TextStyle(color: Colors.grey,fontSize: 13,)),
+                  trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return HeritageList();
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+
+
+
+          Container(
+            margin: const EdgeInsets.only(left: 8,right: 8,top: 8),
+
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Image.asset('assets/images/customer-service.png',),
+                  title: const Text('Help and Support',
+                      style: TextStyle(color: Colors.black,fontSize: 16,)),
+                  subtitle: const Text('always active for safe and hassle free travel',
+                      style: TextStyle(color: Colors.grey,fontSize: 13,)),
+                  trailing: const Icon(Icons.arrow_forward_ios_sharp,size: 14,),
+                ),
+              ),
+            ),
           ),
 
 
---
+++ lib/Pages/HomePage/Haritage/heritage_list.dart
@@ -0,0 +1,77 @@
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeritageList extends StatelessWidget{
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    Query dbref=FirebaseDatabase.instance.ref("Heritage");
+
+
+
+    // TODO: implement build
+    return Scaffold(
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+
+            FirebaseAnimatedList(
+              physics: ScrollPhysics(),
+              shrinkWrap: true,
+              query: dbref,
+              reverse: false,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
+
+                return Container(
+                  margin: EdgeInsets.all(8),
+                  child: Card(
+                    child: ExpansionTile(
+                      //leading: Image.network("${snapshot.child("ImgURL").value.toString()}"),
+                      textColor: Colors.black,
+                      iconColor: Colors.black,
+                      collapsedBackgroundColor: Colors.red.shade100,
+                      title: Text('${snapshot.child("Name").value.toString()}',
+                        style: GoogleFonts.openSans(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+
+                      ),
+                      subtitle: Text('${snapshot.child("address").value.toString()}'),
+                      children: <Widget>[
+                        //Text("${snapshot.child("Name").value.toString()}  \n"),
+                        Container(
+                            padding: EdgeInsets.all(16),
+                            child: Image.network("${snapshot.child("ImgURL").value.toString()}")),
+                        Container(
+                          padding: EdgeInsets.all(16),
+                            child: Text("${snapshot.child("Description").value.toString()}")
+                        ),
+
+                      ],
+                    ),
+                  ),
+                );
+
+              },
+
+            )
+
+          ],
+        ),
+      ),
+
+    );
+  }
+
+}
