6a3eef697db424e206f2deb5dc69b4a30446e2fc
gblbd <gbl.devs@gmail.com>
2023-08-07 15:16:58 +0600

    ✅✅✅ Update at 08/07/2023 PM

---
+++ lib/Pages/HomePage/Bike/Notification Page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotificationPageB extends StatefulWidget {
+  const NotificationPageB({super.key});
+
+  @override
+  State<NotificationPageB> createState() => _NotificationPageBState();
+}
+
+class _NotificationPageBState extends State<NotificationPageB> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
